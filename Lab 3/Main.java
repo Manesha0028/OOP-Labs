17cc2a019ccb504d2383bbfe50cda8d0aa584a2a
HSPLM PATHIRANA <140143628+Manesha0028@users.noreply.github.com>
2023-09-13 08:14:18 +0530

    Add files via upload

--- Lab 3/Main.java
+++ Lab 3/Main.java
@@ -1,1 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        // Create an instance using the default constructor
+        Student student1 = new Student();
+        System.out.println("Student 1 Info:");
+        student1.displayInfo();
+        System.out.println();
 
+        // Create an instance using the parameterized constructor
+        Student student2 = new Student("John", 20, 'A');
+        System.out.println("Student 2 Info:");
+        student2.displayInfo();
+        System.out.println();
+
+        // Create an instance using the constructor with default grade 'N'
+        Student student3 = new Student("Alice", 18);
+        System.out.println("Student 3 Info:");
+        student3.displayInfo();
+    }
+}
