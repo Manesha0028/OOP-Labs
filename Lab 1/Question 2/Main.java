f7b366f20ee554b7b0e6206a3b43e862c962802b
HSPLM PATHIRANA <140143628+Manesha0028@users.noreply.github.com>
2023-09-13 08:06:42 +0530

    Add files via upload

--- Lab 1/Question 2/Main.java
+++ Lab 1/Question 2/Main.java
@@ -1,1 +1,15 @@
+    public class Main {
+        public static void main(String[] args) {
+            
+            Area rectangle = new Area();
+
+            double length = 5;
+            double breadth = 10;
+            rectangle.setDim(length, breadth);
+    
+            double area = rectangle.getArea();
+            System.out.println("Area of the rectangle with length " + length + " and breadth " + breadth + " is: " + area);
+        }
+    }
+    
 
