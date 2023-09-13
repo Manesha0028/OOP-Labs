e7f723e5039e61316d268ba99f4117d150cd54d9
HSPLM PATHIRANA <140143628+Manesha0028@users.noreply.github.com>
2023-09-13 08:20:32 +0530

    Update Student.java

--- Lab 1/Question 1/Student.java
+++ Lab 1/Question 1/Student.java
@@ -1,3 +1,12 @@
-public class Student {
+class Student {
+    private String name;
+
+
+    public Student(String name) {
+        this.name = name;
+    }
     
+    public String getName() {
+        return name;
+    }
 }
